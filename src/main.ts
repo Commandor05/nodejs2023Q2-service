11ac60c2e9faa55d79d6f1937cf24ba94db2b968
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-07-29 16:31:14 +0300

    feat: add swagger and applying port from dot env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import swaggerConf from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 4000;
+
+  const openApiDoc = swaggerConf() as unknown as OpenAPIObject;
+  SwaggerModule.setup('doc', app, openApiDoc);
+
+  await app.listen(port);
 }
 bootstrap();
